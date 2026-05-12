cba99aac34de307e4e3d63b0c813997194930b05
skribster <130120412+skribster@users.noreply.github.com>
2026-05-11 21:29:12 -0700

    Added entry point
    Added an entry point to the application. It now creates a new application and deletes it once done. :)
    
    Signed-off-by: skribster <130120412+skribster@users.noreply.github.com>

---
+++ Pondo/src/Pondo.h
@@ -0,0 +1,17 @@
+#pragma once
+
+/*
+	For use by Pondo applications
+
+	This is so that any application can just do
+	#include <Pondo.h> and get access to all the public headers of Pondo
+*/ 
+
+#include <stdio.h>
+#include "Pondo/Application.h"
+
+// ---Entry Point-----------------
+
+#include "Pondo/EntryPoint.h"
+
+// -------------------------------
---
+++ Pondo/src/Pondo/EntryPoint.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#ifdef PD_PLATFORM_WINDOWS
+
+extern Pondo::Application* Pondo::CreateApplication();
+
+int main(int argc, char** argv) {
+	printf("Hazel Engine\n");
+
+	auto app = Pondo::CreateApplication();
+	app->Run();
+	delete app;
+}
+
+#endif
