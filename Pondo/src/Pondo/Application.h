b02862c614ee7b792c07c3d9f82dc070a2bc895c
skribster <130120412+skribster@users.noreply.github.com>
2026-05-13 21:33:38 -0700

    Added logging functions
    Imported a submodule called SPDLog which allows us to print better errors or diagnose problems easier :)
    
    Signed-off-by: skribster <130120412+skribster@users.noreply.github.com>

--- Pondo/src/Pondo/Application.h
+++ Pondo/src/Pondo/Application.h
@@ -3,6 +3,12 @@
 #include "Core.h"
 
 namespace Pondo {
+	/**
+	 * The class that deals with application initialization, lifetime, and shutdown.
+	 * Any Pondo game will create a class that inherits from this, and implement the 
+	 * CreateApplication() method in their own code (Sandbox) to return a new instance 
+	 * of their class.
+	 */
 	class PONDO_API Application {
 	public:
 		Application();
