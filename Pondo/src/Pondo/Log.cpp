b1b87e9cc0c7d4cfc75e3d292bdaae9973f315cf
LimaGPT <lam41329@eduhsd.k12.ca.us>
2026-06-07 17:30:35 +0200

    Refactor Log initialization and logger setup

--- Pondo/src/Pondo/Log.cpp
+++ Pondo/src/Pondo/Log.cpp
@@ -1,16 +1,17 @@
 #include "Log.h"
 
 namespace Pondo {
-	std::shared_ptr<spdlog::logger> Log::s_coreLogger;
-	std::shared_ptr<spdlog::logger> Log::s_clientLogger;
 
-	void Log::Init() {
-		spdlog::set_pattern("%^[%T] %n: %v%$");
+    std::shared_ptr<spdlog::logger> Log::s_coreLogger;
+    std::shared_ptr<spdlog::logger> Log::s_clientLogger;
 
-		s_coreLogger = spdlog::stdout_color_mt("PONDO");
-		s_coreLogger->set_level(spdlog::level::trace);
+    void Log::Init()
+    {
+        spdlog::set_pattern("%^[%T] %n: %v%$");
+        s_coreLogger = spdlog::stdout_color_mt("PONDO");
+        s_coreLogger->set_level(spdlog::level::trace);
+        s_clientLogger = spdlog::stdout_color_mt("APP");
+        s_clientLogger->set_level(spdlog::level::trace);
+    }
 
-		s_clientLogger = spdlog::stdout_color_mt("APP");
-		s_coreLogger->set_level(spdlog::level::trace);
-	}
 }
