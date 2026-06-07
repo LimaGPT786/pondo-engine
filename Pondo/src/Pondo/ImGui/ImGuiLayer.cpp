b786e47d7cabf1b0d4ae5d7c08ebe4af40b7b994
LimaGPT <lam41329@eduhsd.k12.ca.us>
2026-06-07 17:36:19 +0200

    Add ImGuiLayer class with event handling stub

---
+++ Pondo/src/Pondo/ImGui/ImGuiLayer.cpp
@@ -0,0 +1,14 @@
+#include "ImGuiLayer.h"
+#include "../Events/Event.h"
+
+namespace Pondo {
+
+	ImGuiLayer::ImGuiLayer()
+		: Layer("ImGuiLayer") {
+	}
+
+	void ImGuiLayer::OnEvent(Event& e)
+	{
+	}
+
+}
