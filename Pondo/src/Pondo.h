81f2e4b77cdb2e6e298a8b037336791a064c38ef
LimaGPT <lam41329@eduhsd.k12.ca.us>
2026-06-07 17:32:25 +0200

    Add additional Pondo headers for rendering and scene

--- Pondo/src/Pondo.h
+++ Pondo/src/Pondo.h
@@ -1,17 +1,34 @@
 #pragma once
 
-/*
-	For use by Pondo applications
-
-	This is so that any application can just do
-	#include <Pondo.h> and get access to all the public headers of Pondo
-*/ 
+#include <optional>
 
 #include "Pondo/Application.h"
 #include "Pondo/Log.h"
+#include "Pondo/Layer.h"
+#include "Pondo/Input.h"
+#include "Pondo/Timestep.h"
 
-// ---Entry Point-----------------
+// ---Renderer--------------------
+#include "Pondo/Renderer/Renderer.h"
+#include "Pondo/Renderer/Buffer.h"
+#include "Pondo/Renderer/Shader.h"
+#include "Pondo/Renderer/VertexArray.h"
+#include "Pondo/Renderer/Camera.h"
+#include "Pondo/Renderer/Framebuffer.h"
+#include "Pondo/Renderer/Mesh.h"
+#include "Pondo/Renderer/Material.h"
+// --------------------------------
 
+// ---Scene------------------------
+#include "Pondo/Scene/Components.h"
+#include "Pondo/Scene/Entity.h"
+#include "Pondo/Scene/Scene.h"
+// --------------------------------
+
+// ---ImGui------------------------
+#include "Pondo/ImGui/ImGuiLayer.h"
+// --------------------------------
+
+// ---Entry Point------------------
 #include "Pondo/EntryPoint.h"
-
-// -------------------------------
+// --------------------------------
