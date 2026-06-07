64f67e98ef1f62060577b5cea5e6a642abc0a4d0
LimaGPT <lam41329@eduhsd.k12.ca.us>
2026-06-07 17:29:41 +0200

    Add debug assertion macros for PD_DEBUG
    Added debug assertion macros for error checking.

--- Pondo/src/Pondo/Core.h
+++ Pondo/src/Pondo/Core.h
@@ -1,13 +1,22 @@
 #pragma once
+#include <stdio.h>
 
 #ifdef PD_PLATFORM_WINDOWS
 	#ifdef PD_BUILD_DLL
 		#define PONDO_API __declspec(dllexport)
-	#else 
+	#else
 		#define PONDO_API __declspec(dllimport)
 	#endif
 #else
 	#error Pondo only supports Windows!
+#endif
+
+#ifdef PD_DEBUG
+	#define PD_CORE_ASSERT(x, ...) { if(!(x)) { __debugbreak(); } }
+	#define PD_ASSERT(x, ...)      { if(!(x)) { __debugbreak(); } }
+#else
+	#define PD_CORE_ASSERT(x, ...)
+	#define PD_ASSERT(x, ...)
 #endif
 
 #define BIT(x) (1 << x)
