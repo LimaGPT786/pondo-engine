2c89a047d1942e62ab2ac8a2b5887a59d937ce66
LimaGPT <lam41329@eduhsd.k12.ca.us>
2026-06-07 17:28:42 +0200

    Refactor Application class and add layer management

--- Pondo/src/Pondo/Application.cpp
+++ Pondo/src/Pondo/Application.cpp
@@ -1,41 +1,82 @@
 #include "Application.h"
+#include "Input.h"
 #include "Log.h"
+#include <GLFW/glfw3.h>
 
 namespace Pondo {
-    Application::Application()
-    {
-        m_window = new Window({ "Pondo Engine", 1280, 720, false });
-        m_window->Init();
-        m_window->SetEventCallback([this](Event& e) { OnEvent(e); });
-    }
 
-    Application::~Application()
-    {
-        delete m_window;
-    }
+	Application* Application::s_Instance = nullptr;
 
-    void Application::OnEvent(Event& e)
-    {
-        EventDispatcher dispatcher(e);
-        dispatcher.Dispatch<WindowCloseEvent>([this](WindowCloseEvent& e) {
-            return OnWindowClose(e);
-            }
-        );
+	Application::Application()
+	{
+		s_Instance = this;
 
-        PD_CORE_TRACE("{0}", e.ToString());
-    }
+		m_Window = new Window({ "Pondo Engine", 1280, 720, false });
+		m_Window->Init();
+		m_Window->SetEventCallback([this](Event& e) { OnEvent(e); });
+	}
 
-    bool Application::OnWindowClose(WindowCloseEvent& e)
-    {
-        m_running = false;
-        return true;
-    }
+	Application::~Application()
+	{
+		delete m_Window;
+	}
 
-    void Application::Run()
-    {
-        while (m_running)
-        {
-            m_window->OnUpdate();
-        }
-    }
+	void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+	}
+
+	void Application::PushOverlay(Layer* overlay)
+	{
+		m_LayerStack.PushOverlay(overlay);
+	}
+
+	void* Application::GetProcAddress(const char* name)
+	{
+		return (void*)glfwGetProcAddress(name);
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		PD_CORE_TRACE("{0}", e.ToString());
+
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>([this](WindowCloseEvent& e) {
+			return OnWindowClose(e);
+			});
+
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
+		{
+			(*it)->OnEvent(e);
+			if (e.Handled)
+				break;
+		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+	void Application::Run()
+	{
+		float lastFrameTime = 0.0f;
+
+		while (m_Running)
+		{
+			float time = (float)glfwGetTime();
+			Timestep timestep = time - lastFrameTime;
+			lastFrameTime = time;
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate(timestep);
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnRender();
+
+			m_Window->OnUpdate();
+		}
+	}
+
 }
